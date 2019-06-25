a2047312c428dd4adb34e659057401490aab8b70
Desiree Chon <ddchon@gmail.com>
2019-06-25 14:00:36 -0400

    got folders and files set up

---
+++ zenofpythonpuzzle/zenpythonpages/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse('This is homepage.')
+
+def new_user(request):
+    return HttpResponse('This is new user page.')
+
+def profile(request):
+    return HttpResponse('This is profile page.')
+
+def submit_q(request):
+    return HttpResponse('This is submit question page.')
+
+def submit_comp(request):
+    return HttpResponse('This is submit complete page.')
+
