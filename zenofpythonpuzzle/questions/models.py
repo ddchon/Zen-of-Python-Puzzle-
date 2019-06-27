70ab50b1bb76d75b64b77e5969fb90cc3f7ecb86
rafi.alvi821 <rafi.alvi821@gmail.com>
2019-06-27 13:03:21 -0400

    6/27 update

--- zenofpythonpuzzle/questions/models.py
+++ zenofpythonpuzzle/questions/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    question = models.TextField()
+    p_answer1 = models.TextField()
+    p_answer2 = models.TextField()
+    p_answer3 = models.TextField()
+    p_answer4 = models.TextField()
+    answer = models.TextField()
+
+    def __str__(self):
+        return f"{self.id}.  {self.question}"
