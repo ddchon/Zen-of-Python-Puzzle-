56b670a41c4e761913e9b13d702ed63d82dbd181
Desiree Chon <ddchon@gmail.com>
2019-06-27 16:15:19 -0400

    updating everyone

--- zenofpythonpuzzle/questions/views.py
+++ zenofpythonpuzzle/questions/views.py
@@ -12,11 +12,11 @@ def ind_questions(request, id):
         print(question)
         return render(request, "questions/ind_question.html", context=context)
 
-def submitted_answer(request):
-        if request.method == "POST":
-        user_answer = new_question.answer
+# def submitted_answer(request):
+#         if request.method == "POST":
+#         user_answer = new_question.answer
 
-        if user_answer != 
+#         if user_answer != 
 
 def new_question(request):
     if request.method == "POST":
