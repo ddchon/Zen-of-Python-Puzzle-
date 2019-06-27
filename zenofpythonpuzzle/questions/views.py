98ab038c57610668f1a35af87ad37746e3f084fc
rafi.alvi821 <rafi.alvi821@gmail.com>
2019-06-27 15:11:32 -0400

    2nd push 6/27

--- zenofpythonpuzzle/questions/views.py
+++ zenofpythonpuzzle/questions/views.py
@@ -4,18 +4,29 @@ from .models import Question
 def q_home(request):
     return render(request, "questions/questionshome.html")
 
-def ind_questions(request):
-    return render(request, "questions/ind_question.html")
+def ind_questions(request, id):
+        question = Question.objects.get(id=id)
+        context = {
+                "question" : question
+        }
+        print(question)
+        return render(request, "questions/ind_question.html", context=context)
+
+def submitted_answer(request):
+        if request.method == "POST":
+        user_answer = new_question.answer
+
+        if user_answer != 
 
 def new_question(request):
     if request.method == "POST":
-        new_question = Question (question=request.POST["question"],
+        question_list = Question (question=request.POST["question"],
                                  p_answer1=request.POST["p_answer1"],
                                  p_answer2=request.POST["p_answer2"],
                                  p_answer3=request.POST["p_answer3"],
                                  p_answer4=request.POST["p_answer4"],
                                  answer=request.POST["answer"])
-        new_question.save()
+        question_list.save()
         return redirect("q_home")
 
 def q_success(request):
